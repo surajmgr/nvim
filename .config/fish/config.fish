5579d62de2a1e507289bd8928555bd411b913e97
Suraj Pulami <surajpulami8@gmail.com>
2026-01-22 10:38:26 +0545

    fix: caesium

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,15 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
 source ~/.config/fish/functions/custom_alias.fish
+
+# overwrite prompt
+function fish_prompt
+    echo (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) '> '
+end
 
 # overwrite greeting
 # potentially disabling fastfetch
 function fish_greeting
+  return
     set_color cyan
     for i in (seq (random 3 10))   # random number of fish between 3 and 10
         for j in (seq (random 1 40))   # random spacing before fish
--- .config/fish/functions/custom_alias.fish
+++ .config/fish/functions/custom_alias.fish
@@ -12,3 +12,17 @@ end
 function ani
     ~/.dotfiles/.config/custom/ani-cli-wrapper/launcher.sh
 end
+
+function deb_up
+    set vm_path "/Users/surajpulami/Virtual Machines.localized/Debian 13.x 64-bit Arm.vmwarevm/Debian 13.x 64-bit Arm.vmx"
+    /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion start $vm_path nogui
+end
+
+function deb_down
+    set vm_path "/Users/surajpulami/Virtual Machines.localized/Debian 13.x 64-bit Arm.vmwarevm/Debian 13.x 64-bit Arm.vmx"
+    /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion stop $vm_path soft
+end
+
+function vm_list
+    /Applications/VMware\ Fusion.app/Contents/Library/vmrun list
+end
